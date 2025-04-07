ad8302f8b22dd033cca5081f74ed5fe17deb45f6
Ohannes Hajjar <ohanneshajjar@gmail.com>
2025-04-07 02:21:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,3 @@ export default defineConfig({
   },
 })
 
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
